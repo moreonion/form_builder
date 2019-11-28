fd5b9c2041f8fe08173f1524d34ba3ee2d6a53fa
Roman Zimmermann <roman@more-onion.com>
2019-11-28 22:07:44 +0100

    Use webform’s configuration for the maxlength property.

---
+++ modules/webform/src/PropertyMaxlength.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\form_builder_webform;
+
+/**
+ * Extend maxlength property for better UX.
+ */
+class PropertyMaxlength extends Property {
+
+  /**
+   * Generate form-API elements for editing this property.
+   */
+  public function form($component, $edit, &$form_state) {
+    return [
+      '#element_validate' => 'form_validate_integer',
+      '#field_suffix' => ' ' . t('characters'),
+    ] + parent::form($component, $edit, $form_state);
+  }
+
+}
